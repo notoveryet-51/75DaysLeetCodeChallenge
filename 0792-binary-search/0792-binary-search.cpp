938cb3630cc3987c5303d88884b8112896cd6969
Sandipan Ray <sray387.official@gmail.com>
2026-04-03 08:39:43 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.82%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int high=n-1, low=0;
+        while (low<=high)    {
+            int mid=(low+high)/2;
+            if (nums[mid]<target)  low=mid+1;
+            else if(nums[mid]==target)  return mid;
+            else high=mid-1;
+        }
+        return -1;
+    }
+};
