dd9fd26f6be963aa1cc3d51ba5c4606689d8da00
Sandipan Ray <sray387.official@gmail.com>
2026-04-03 08:33:50 +0530

    Time: 0 ms (100%), Space: 48.6 MB (22.16%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int high=n-1, low=0;
+        while (low<=high)    {
+            int mid=(low+high)/2;
+            if (nums[mid]<target)  low=mid+1;
+            else if(nums[mid]==target)  return mid;
+            else high=mid-1;
+        }
+        return -1;
+    }
+}
