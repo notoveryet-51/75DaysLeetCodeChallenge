9ab475387a0bac0c9d5076c6dad0287020188b60
Sandipan Ray <sray387.official@gmail.com>
2026-06-01 09:16:56 +0530

    Time: 0 ms (100%), Space: 41.3 MB (99.41%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a=0,b=1;
+        int sum=0;
+        for (int i=1; i<=n; i++) {
+        	sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+    }
+}
